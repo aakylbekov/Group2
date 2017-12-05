06203f58b9771bd7431fc8992ab568769d32c84d
MuslimBeibytuly <muslimbeibytuly@gmail.com>
2017-12-05 13:40:51 +0600

    virtual functions

---
+++ Group2/Example/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> x = new List<int>();
+            x.Add(2);
+            Console.WriteLine(x[0]);
+        }
+    }
+}
